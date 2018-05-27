a525a621c1d7938ec49c5a16b844a7068a6e3bb5
saminwankwo <nwankwosami@gmail.com>
2018-05-27 12:16:17 +0100

    finished my home page

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8" name="viewport" content="width=device-width"/>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
-		<title>PHP - Simple Barcode Generator</title>
+		<meta charset="UTF-8" name="viewport" content="width=device-width"/>
+		<title>PHP - Simple Barcode Generator by Nwankwo Samuel</title>
 	</head>
-	<body>
-		<nav class = "navbar navbar-default">
-			<div class = "container-fluid" >
-				<a href="dmjku">Samuel Nwankwo</a>			
-			</div>
-		</nav>
-
-		<div class = "col-md-3"></div>
-		<div class = "col-md6 well">
-			<h3 class="text-primary">PHP - Simple Barcode Generator</h3>
-			<hr style="border-top:1px dotted #ccc;"/>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<a class="navbar-brand" <a href="https://facebook.com/nwankwo.samuel">Samuel Nwankwo</a>
 		</div>
-	
-	</body>
-	</html>
+	</nav>
+	<div class="col-md-3"></div>
+	<div class="col-md-6 well">
+		<h3 class="text-primary">PHP - Simple Barcode Generator</h3>
+		<hr style="border-top:1px dotted #ccc;"/>
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<form method="POST">
+				<div class="form-group">
+					<label>Enter a text</label>
+					<input type="text" class="form-control" name="barcode"/>
+					<br />
+					<center><button class="btn btn-primary" name="generate">Generate</button></center>
+					<br />
+					<?php include 'generate.php';?>
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
 </html>
