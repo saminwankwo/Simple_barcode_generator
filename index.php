2c3ed64c4fb06cc438b0f1070013be32839b2557
saminwankwo <nwankwosami@gmail.com>
2018-05-27 11:57:29 +0100

    added index page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" name="viewport" content="width=device-width"/>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+		<title>PHP - Simple Barcode Generator</title>
+	</head>
+	<body>
+		<nav class = "navbar navbar-default">
+			<div class = "container-fluid" >
+				<a href="dmjku">Samuel Nwankwo</a>			
+			</div>
+		</nav>
+
+		<div class = "col-md-3"></div>
+		<div class = "col-md6 well">
+			<h3 class="text-primary">PHP - Simple Barcode Generator</h3>
+			<hr style="border-top:1px dotted #ccc;"/>
+		</div>
+	
+	</body>
+	</html>
+</html>
